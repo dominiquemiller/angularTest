a002777db530216e05e1883eca65160fcceed51b
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-11 13:52:27 -0500

    added funcky cold medena product

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+(function() {
 var app = angular.module('store', []);
+
+app.controller('StoreController', function(){
+  this.product = gem;
+});
+  var gem = {
+    name: 'Funky Cold Madena',
+    price: 2000,
+    description: 'dis dat super duper fly stuff!',
+  };
+
+})();
