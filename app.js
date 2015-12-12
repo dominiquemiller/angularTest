adebcb2ddbd6678212162303bfcd25f4bf397fa3
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-12 09:03:51 -0500

    added more test content

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var app = angular.module('store', []);
 app.controller('StoreController', function(){
   this.product = gem;
 });
-  var gem = {
+  var gem = [{
     name: 'Funky Cold Madena',
     price: 2000,
     description: 'dis dat super duper fly stuff!',
     soldOut: false,
-  };
+  },
+  { name: 'Another gem', price: 150, description: 'really generic gem', },
+  { name: 'cubic sillyness', price: 300, description: 'mid grade generic',}
+];
 
 })();
