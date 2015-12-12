d7822532894d9007a1fbd51087a6bd62fc317cda
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-12 09:45:08 -0500

    display product review

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ app.controller('StoreController', function(){
   this.products = gems;
 });
   var gems = [
-  { name: 'Funky Cold Madena', price: 2000, description: 'dis dat super duper fly stuff!', soldOut: false },
-  { name: 'Another gem', price: 150, description: 'really generic gem' },
-  { name: 'cubic sillyness', price: 300, description: 'mid grade generic' }
+  { name: 'Funky Cold Madena', price: 2000, description: 'dis dat super duper fly stuff!', soldOut: false, reviews: [ {
+                                                                                                                    stars: 5,
+                                                                                                                    body: "none",
+                                                                                                                    author: "me",
+                                                                                                                    createdOn: 1397490980837
+                                                                                                                  } ] },
+  { name: 'Another gem', price: 150, description: 'really generic gem', reviews: [] },
+  { name: 'cubic sillyness', price: 300, description: 'mid grade generic', reviews: [] }
 ];
 
 })();
