73365991f4338e94240dd89e681dec4a75a415ff
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-12 09:11:51 -0500

    added product rows and ng-repeat to gems

--- app.js
+++ app.js
@@ -2,16 +2,12 @@
 var app = angular.module('store', []);
 
 app.controller('StoreController', function(){
-  this.product = gem;
+  this.products = gems;
 });
-  var gem = [{
-    name: 'Funky Cold Madena',
-    price: 2000,
-    description: 'dis dat super duper fly stuff!',
-    soldOut: false,
-  },
-  { name: 'Another gem', price: 150, description: 'really generic gem', },
-  { name: 'cubic sillyness', price: 300, description: 'mid grade generic',}
+  var gems = [
+  { name: 'Funky Cold Madena', price: 2000, description: 'dis dat super duper fly stuff!', soldOut: false },
+  { name: 'Another gem', price: 150, description: 'really generic gem' },
+  { name: 'cubic sillyness', price: 300, description: 'mid grade generic' }
 ];
 
 })();
